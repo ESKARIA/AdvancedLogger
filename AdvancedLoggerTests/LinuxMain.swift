6ecd261a94a8d045211df965868b038ed3bdb701
Dmitriy <ToropkinD@gmail.com>
2020-04-15 00:08:12 +0300

    [spm] test for linux

--- AdvancedLoggerTests/LinuxMain.swift
+++ AdvancedLoggerTests/LinuxMain.swift
@@ -2,8 +2,10 @@ import XCTest
 
 import ALCryptoTest
 import ALFileManagerTest
+import AdvancedLoggerTest
 
 var tests = [XCTestCase]()
 tests += ALCryptoTest.allTests()
 tests += ALFileManagerTest.allTests()
+tests += AdvancedLoggerTest.allTests()
 XCTMain(tests)
