b24256528ca0af2ebf5782f424ed00889295367c
Dmitriy <ToropkinD@gmail.com>
2020-04-15 00:17:18 +0300

    delete queue

--- AdvancedLoggerTests/Tests/AdvancedLoggerTest.swift
+++ AdvancedLoggerTests/Tests/AdvancedLoggerTest.swift
@@ -12,75 +12,65 @@ import XCTest
 class AdvancedLoggerTest: XCTestCase {
     
     var testLog: String!
-    var queue: DispatchQueue!
     
     override func setUp() {
         super.setUp()
         self.testLog = "AdvancedLogger test write"
-        self.queue =  DispatchQueue(label: "com.AdvancedLogger.Test")
     }
     
     override func tearDown() {
         super.tearDown()
         self.testLog = nil
-        self.queue = nil
     }
     
     func testWrite() {
-        queue.sync {
-            AdvancedLogger.shared.encryptData = false
-            AdvancedLogger.shared.addNew(log: self.testLog, type: .execution)
-            
-            AdvancedLogger.shared.getStringLogs { (log) in
-                XCTAssertNotNil(log, "Error with write log. Logger return nil after write!")
-                XCTAssertEqual(self.testLog, log!)
-            }
-            
-            AdvancedLogger.shared.cleanLogs()
+        AdvancedLogger.shared.encryptData = false
+        AdvancedLogger.shared.addNew(log: self.testLog, type: .execution)
+        
+        AdvancedLogger.shared.getStringLogs { (log) in
+            XCTAssertNotNil(log, "Error with write log. Logger return nil after write!")
+            XCTAssertEqual(self.testLog, log!)
         }
+        AdvancedLogger.shared.cleanLogs()
     }
     
     func testCryptoWrite() {
-        queue.sync {
-            AdvancedLogger.shared.encryptData = true
-            AdvancedLogger.shared.addNew(log: self.testLog, type: .execution)
-            
-            AdvancedLogger.shared.getStringLogs { (log) in
-                XCTAssertNotNil(log, "Error with write log. Logger return nil after write!")
-                XCTAssertEqual(self.testLog, log!)
-            }
-            
-            AdvancedLogger.shared.cleanLogs()
+        AdvancedLogger.shared.encryptData = true
+        AdvancedLogger.shared.addNew(log: self.testLog, type: .execution)
+        
+        AdvancedLogger.shared.getStringLogs { (log) in
+            XCTAssertNotNil(log, "Error with write log. Logger return nil after write!")
+            XCTAssertEqual(self.testLog, log!)
         }
+        
+        AdvancedLogger.shared.cleanLogs()
     }
     
     func testUpdateCryptoKeys() {
-        queue.sync {
-            
-            var dataWithDefaultKey: Data?
-            var dataWithCustomtKey: Data?
-            
-            AdvancedLogger.shared.encryptData = true
-            AdvancedLogger.shared.addNew(log: self.testLog, type: .execution)
-            
-            AdvancedLogger.shared.getDataLogs { (data) in
-                XCTAssertNotNil(data, "Error with write log. Logger return nil after write!")
-                dataWithDefaultKey = data
-            }
-            
-            AdvancedLogger.shared.cleanLogs()
-            
-            AdvancedLogger.shared.aesCryptoKeys = ALAESCryptoInitModel(cryptoKey: "98765432101234567890123456789012", initialVector: "zxcdefghijklmnop")
-            AdvancedLogger.shared.addNew(log: self.testLog, type: .execution)
-            
-            AdvancedLogger.shared.getDataLogs { (data) in
-                XCTAssertNotNil(data, "Error with write log. Logger return nil after write!")
-                dataWithCustomtKey = data
-            }
-            
-            XCTAssertEqual(dataWithDefaultKey, dataWithCustomtKey)
-            
-            AdvancedLogger.shared.cleanLogs()
+        
+        var dataWithDefaultKey: Data?
+        var dataWithCustomtKey: Data?
+        
+        AdvancedLogger.shared.encryptData = true
+        AdvancedLogger.shared.addNew(log: self.testLog, type: .execution)
+        
+        AdvancedLogger.shared.getDataLogs { (data) in
+            XCTAssertNotNil(data, "Error with write log. Logger return nil after write!")
+            dataWithDefaultKey = data
         }
+        
+        AdvancedLogger.shared.cleanLogs()
+        
+        AdvancedLogger.shared.aesCryptoKeys = ALAESCryptoInitModel(cryptoKey: "98765432101234567890123456789012", initialVector: "zxcdefghijklmnop")
+        AdvancedLogger.shared.addNew(log: self.testLog, type: .execution)
+        
+        AdvancedLogger.shared.getDataLogs { (data) in
+            XCTAssertNotNil(data, "Error with write log. Logger return nil after write!")
+            dataWithCustomtKey = data
+        }
+        
+        XCTAssertEqual(dataWithDefaultKey, dataWithCustomtKey)
+        
+        AdvancedLogger.shared.cleanLogs()
     }
 }
